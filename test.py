27672205ebad5f9e0d286ee98721cb5045d0b998
Justin <JTP75@pitt.edu>
2023-03-06 17:03:43 -0500

    /home/pacel/anaconda3/envs/env0/bin/python

--- test.py
+++ test.py
@@ -3,20 +3,6 @@ import pygame
 import numpy as np
 import matplotlib.pyplot as plt
 
-
-
-'''
-im = plt.imread("lib/bin/s.png")
-plt.imshow(im)
-plt.xlim((195,249))
-plt.ylim((301,247))
-plt.show()
-'''
-
-wall = plt.imread("lib/bin/wall.png")
-plt.imshow(wall)
-plt.show()
-
 pygame.init()
 rect = pygame.Rect(10,20,30,30)
 #dsp = pygame.display.set_mode((800,800))
